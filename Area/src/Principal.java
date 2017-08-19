e0b5202187e14b9cf1739e203f1483a0dfff1dc4
felipeprintes <felipe.unilasalle@gmail.com>
2017-08-19 13:42:54 -0300

    POO

--- Area/src/Principal.java
+++ Area/src/Principal.java
@@ -1,20 +1,28 @@
+import java.util.Scanner;
 
 public class Principal {
 	
 	public static void main(String[] args) {
 		
+		Scanner in = new Scanner(System.in);
+		
 		Triangulo t1 = new Triangulo();
-		t1.setBase(1.0);
+		/*t1.setBase(1.0);
 		t1.setAltura(1.0);
 		
 		t1.almentaAltura(4);
-		t1.almentaBase(4);
+		t1.almentaBase(4);*/
 		
 		
 		
-		System.out.println("O valor da Area e: " + t1.getArea());
+		System.out.println("entre com a base: ");
+		t1.setBase(in.nextInt());
+		
+		System.out.println(t1.getBase());
+		
+		/*System.out.println("O valor da Area e: " + t1.getArea());
 		System.out.println("O valor da sua Altura e: " + t1.getAltura());
-		System.out.println("O valor da sua base e: " + t1.getBase());
+		System.out.println("O valor da sua base e: " + t1.getBase());*/
 		
 	}
 
