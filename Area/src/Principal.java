b916f50644f360723a227bb2583d5ed6394a65bd
felipeprintes <felipe.unilasalle@gmail.com>
2017-08-20 09:37:08 -0300

    area

--- Area/src/Principal.java
+++ Area/src/Principal.java
@@ -7,18 +7,25 @@ public class Principal {
 		Scanner in = new Scanner(System.in);
 		
 		Triangulo t1 = new Triangulo();
-		/*t1.setBase(1.0);
-		t1.setAltura(1.0);
+		//t1.setBase(1.0);
+		//t1.setAltura(1.0);
 		
-		t1.almentaAltura(4);
-		t1.almentaBase(4);*/
+		
+		
 		
 		
 		
 		System.out.println("entre com a base: ");
 		t1.setBase(in.nextInt());
+		t1.almentaBase(4);
 		
-		System.out.println(t1.getBase());
+		System.out.println("Entre com a altura: ");
+		t1.setAltura(in.nextInt());
+		t1.almentaAltura(4);
+		
+		System.out.println("A base e igual: " + t1.getBase());
+		System.out.println("A altura e igual: " + t1.getAltura());
+		System.out.println("A area e igual a: " + t1.getArea());
 		
 		/*System.out.println("O valor da Area e: " + t1.getArea());
 		System.out.println("O valor da sua Altura e: " + t1.getAltura());
